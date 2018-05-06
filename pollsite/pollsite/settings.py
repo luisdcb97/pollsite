a7b9a4e2389575d163d85a062c13e5fde0e780dd
Luis <luisdcb97@gmail.com>
2018-05-06 05:02:28 +0100

    Add localhost as allowed host

--- pollsite/pollsite/settings.py
+++ pollsite/pollsite/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'g2tk-&enfbv(-vr68065+l503u+nor*!og(302$onwni$w$$sz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
